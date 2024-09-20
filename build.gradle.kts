49fbd97c0e3071294727ef98378addda8c287b3f
seunghyun <sh.han@sqisoft.com>
2024-09-20 18:31:37 +0900

    fix: 서버 올릴 때 스프링 버전 충돌

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.25" apply false
     kotlin("plugin.spring") version "1.9.25" apply false
-    id("org.springframework.boot") version "3.3.3" apply false
+    id("org.springframework.boot") version "3.2.3" apply false
     id("io.spring.dependency-management") version "1.1.6" apply false
 }
 
