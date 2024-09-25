b34521f90c8a8068b841e3260ad2cf1d1c027432
seunghyun <sh.han@sqisoft.com>
2024-09-25 16:21:24 +0900

    chore: config-server 로 설정 분리

--- config-server/src/main/kotlin/io/ethan/chatting/configserver/ConfigServerApplication.kt
+++ config-server/src/main/kotlin/io/ethan/chatting/configserver/ConfigServerApplication.kt
@@ -3,8 +3,10 @@ package io.ethan.chatting.configserver
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableDiscoveryClient
 class ConfigServerApplication
 
