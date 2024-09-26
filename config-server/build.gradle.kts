74e861741f17cff365263fc35c98c4b56348aa9e
seunghyun <sh.han@sqisoft.com>
2024-09-26 16:58:15 +0900

    chore: config 관련 내용 수정
    repository 에서 설정 값 가져오는 중

--- config-server/build.gradle.kts
+++ config-server/build.gradle.kts
@@ -25,6 +25,7 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.cloud:spring-cloud-config-server")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
