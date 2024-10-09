72cce97137ea52fc57c8fb8163360997ed7430a7
keumminseop <mskeum93@naver.com>
2024-10-09 20:34:48 +0900

    feat: 채팅 도메인 서버 생성

---
+++ chat-service/src/test/kotlin/org/keumin/chatservice/ChatServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.keumin.chatservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
