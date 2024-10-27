db04c852025e5b175139897d596e5fada3a6da6c
seunghyunhan95 <sh.han@sqisoft.com>
2024-10-27 21:48:55 +0900

    chore: api-gateway 설정값 추가

--- api-gateway/src/main/kotlin/io/ethan/chatting/apigateway/ApiGatewayApplication.kt
+++ api-gateway/src/main/kotlin/io/ethan/chatting/apigateway/ApiGatewayApplication.kt
@@ -1,11 +1,14 @@
 package io.ethan.chatting.apigateway
 
+import io.ethan.chatting.apigateway.security_config.SecurityProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigurationProperties(SecurityProperties::class)
 class ApiGatewayApplication
 
 fun main(args: Array<String>) {
